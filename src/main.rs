90fdd5647938476f9eea2d5a3cc0a585026e2879
1363558876@qq.com <1363558876@qq.com>
2021-05-20 22:49:29 +0800

    添加命令提示

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use clap::{App, Arg};
 
 use crate::{maker::Maker, tianmu::TianMu};
 
@@ -7,14 +7,36 @@ mod require;
 mod tianmu;
 mod test;
 
+#[allow(dead_code)]
 const SIZE : usize = 1024 * 1024 * 32;
 
 fn main() {
-    assert!(env::args().len() == 3);
-    let args : Vec<String> = env::args().collect();
-    let src = args.get(1).unwrap();
-    let target = args.get(2).unwrap();
-    let format = TianMu::new(4096 * 4, SIZE);
+    let matches = App::new("Image maker")
+        .version("0.1.3")
+        .author("大树之下 <belowthetree>")
+        .about("Make an image from a directory with custom format")
+        .arg(Arg::with_name("size")
+            .short("S")
+            .help("Set the image size(mb)")
+            .default_value("32")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("source")
+            .short("s")
+            .help("Set the source directory")
+            .takes_value(true)
+        )
+        .arg(Arg::with_name("target")
+            .short("t")
+            .help("path that image will be placed")
+            .default_value("./")
+            .takes_value(true)
+        ).get_matches();
+    let size = matches.value_of("size").unwrap();
+    let size = from_str(size);
+    let src = matches.value_of("source").unwrap().to_string();
+    let target = matches.value_of("target").unwrap().to_string();
+    let format = TianMu::new(4096 * 4, 1024 * 1024 * size);
     let mut mk = Maker::new(src.clone(), target.clone(), format);
     mk.make();
 }
@@ -29,4 +51,16 @@ mod tests {
             String::from("./"), String::from("img"), format);
         mk.print_super_block();
     }
+}
+
+fn from_str(s : &str)->usize {
+    let mut rt = 0;
+    for c in s.as_bytes() {
+        rt *= 10;
+        if *c < '0' as u8 || *c > '9' as u8 {
+            panic!("size with wrong char {}", s);
+        }
+        rt += *c as usize - '0' as usize;
+    }
+    rt
 }
